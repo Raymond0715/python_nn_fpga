50bae856f9c41a12c693998c09b6c4997922882f
RaymondCh <3160103844@zju.edu.cn>
2022-04-15 13:18:38 +0800

    convert_out_sdk.py add last two convert code.

--- convert_out_sdk.py
+++ convert_out_sdk.py
@@ -4,29 +4,40 @@ import utils
 
 
 # dat_raw_path = Path('.') / 'dat' / 'yolo_tiny_pass_version' / 'out_yolo_bench1_l16_process.dat'
-dat_raw_path = Path('.') / 'dat' / 'yolo' / 'out2_yolo_bench1_l16_process.dat'
-dat_path = Path('.') / 'dat' / 'o3_pc_process.txt'
+# dat_raw_path = Path('.') / 'dat' / 'yolo' / 'out2_yolo_bench1_l16_process.dat'
+# dat_path = Path('.') / 'dat' / 'o3_pc_process.txt'
 
 # dat_raw_path = Path('.') / 'dat' / 'out_test_process.dat'
 # dat_path = Path('.') / 'dat' / 'out_test_process_txt.txt'
 # dat_raw_path = Path('.') / 'dat' / 'out_l9l10_pc_process.dat'
 # dat_path = Path('.') / 'dat' / 'out_l9l10_pc_process_txt.txt'
 
-# dat_raw_path = Path('.') / 'dat' / 'o1.bin'
-# dat_path = Path('.') / 'dat' / 'o1_txt.txt'
+dat_raw_path = Path('.') / 'dat' / 'out_yolov2.dat'
+dat_path = Path('.') / 'dat' / 'out_yolov2_txt_temp.txt'
 
+# FPGA
 out_raw = np.fromfile(dat_raw_path, dtype=np.int32)
+# PC
 # out_raw = np.fromfile(dat_raw_path, dtype=np.int16)
-out_reshape = np.reshape(out_raw, (-1,8))
 
-out_fliplr = np.fliplr(out_reshape)
-out_final = np.copy(out_fliplr.astype(np.int16))
+# out_reshape = np.reshape(out_raw, (-1,8))
+
+# FPGA Last two
+out_reshape = np.reshape(out_raw, (13, 16, 8, 13, 8))
+out_trans = np.swapaxes(out_reshape, 0, 1)
+
+# PC
+# out_fliplr = np.fliplr(out_reshape)
+# out_final = np.copy(out_fliplr.astype(np.int16))
+
+# FPGA
 # out_final = out_reshape
+# FPGA Last two
+out_final = np.copy(out_trans)
 
+# Max pool
 # out_2d = np.copy(out_fliplr.astype(np.int16))
-
 # out_4d = np.reshape(out_2d, (26, 32, 26, 8))
-
 # out_final = np.copy(out_4d[0:-1:2, :, 0:-1:2, :])
 
 # with open(str(dat_path), mode='wb') as f:
